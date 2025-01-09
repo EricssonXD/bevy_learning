398d78f7700fe1258b5aa37c9d34e15cc5706f8b
EricssonXD <choericsson@gmail.com>
2025-01-10 00:14:10 +0800

    ep1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
-use bevy::prelude::*;
+use bevy::{ecs::query, prelude::*, ui::update};
+
+#[derive(Component, Debug)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component, Debug)]
+struct Velocity {
+    x: f32,
+    y: f32,
+}
 
 fn main() {
-    App::new().run();
+    App::new()
+    .add_systems(Startup, spawn_spaceship)
+    .add_systems(Update, (update_position, print_position))
+    .add_plugins(DefaultPlugins)
+    .run();
+}
+
+fn spawn_spaceship(mut commands:Commands) {
+    commands.spawn((Position { x: 0.0, y: 0.0 }, Velocity { x: 1.0, y: 1.0 }));
+    commands.spawn((Position { x: 0.0, y: 0.0 }, Velocity { x: 1.0, y: 1.0 }));
+    commands.spawn((Position { x: 0.0, y: 0.0 }, Velocity { x: 1.0, y: 1.0 }));
+}
+
+fn update_position(mut query: Query<(&mut Position, &Velocity)>) {
+    for (mut position, velocity) in query.iter_mut() {
+        position.x += velocity.x;
+        position.y += velocity.y;
+    }
+}
+
+fn print_position(query:Query<(Entity, &Position)>){
+    for (entity, position) in query.iter() {
+        info!("{:?} - {:?}", entity, position);
+    }
 }
