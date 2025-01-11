701dad31cba6a726e12de5bde13e8c4136c0e57f
EricssonXD <choericsson@gmail.com>
2025-01-11 16:55:44 +0800

    shoot bullets

--- src/spaceship.rs
+++ src/spaceship.rs
@@ -10,16 +10,21 @@ const STARTING_TRANSLATION: Vec3 = Vec3::new(0., 0., -20.);
 const SPACESHIP_SPEED: f32 = 25.0;
 const SPACESHIP_ROTATION_SPEED: f32 = 2.5;
 const SPACESHIP_ROLL_SPEED: f32 = 2.5;
-
+const MISSILE_SPEED: f32 = 50.0;
 #[derive(Component, Debug)]
 pub struct SpaceShip;
+
+#[derive(Component, Debug)]
+pub struct SpaceShipMissile;
 
 pub struct SpaceShipPlugin;
 
 impl Plugin for SpaceShipPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(PostStartup, spawn_spaceship)
-            .add_systems(Update, spaceship_movement_controls);
+        app.add_systems(PostStartup, spawn_spaceship).add_systems(
+            Update,
+            (spaceship_movement_controls, spaceship_weapoin_controls),
+        );
     }
 }
 
@@ -36,11 +41,11 @@ fn spawn_spaceship(mut commands: Commands, asset_server: Res<SceneAssets>) {
 }
 
 fn spaceship_movement_controls(
-    mut query: Query<(&mut Transform, &mut Velocity, &mut Acceleration), With<SpaceShip>>,
+    mut query: Query<(&mut Transform, &mut Velocity), With<SpaceShip>>,
     input: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
 ) {
-    let (mut transform, mut velocity, mut acceleration) = query.single_mut();
+    let (mut transform, mut velocity) = query.single_mut();
     let mut movement = 0.0;
     let mut rotation = 0.0;
     let mut roll = 0.0;
@@ -67,4 +72,24 @@ fn spaceship_movement_controls(
     transform.rotate_y(rotation);
     transform.rotate_local_z(roll); // Barrel Roll Effect
     velocity.value = -transform.forward() * movement;
+}
+
+fn spaceship_weapoin_controls(
+    mut commands: Commands,
+    input: Res<ButtonInput<KeyCode>>,
+    scene_assets: Res<SceneAssets>,
+    query: Query<&Transform, With<SpaceShip>>,
+) {
+    let transform = query.single();
+    if input.pressed(KeyCode::Space) {
+        commands.spawn((
+            MovingObjectBundle {
+                velocity: Velocity::new(-transform.forward() * MISSILE_SPEED),
+                acceleration: Acceleration::new(Vec3::ZERO),
+                model: SceneRoot(scene_assets.missiles.clone()),
+                transform: Transform::from_translation(transform.translation),
+            },
+            SpaceShipMissile,
+        ));
+    }
 }
